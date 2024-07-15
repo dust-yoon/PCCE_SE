5fce1b1a860316464d0f53028bc505610e37ab1a
JIYUN <popowjy12@konkuk.ac.kr>
2024-07-16 00:02:56 +0900

    docs: 문서 수정

--- test.py
+++ test.py
@@ -2,6 +2,8 @@
 #  프로그램 언어가 아닐 경우, 주석 처리를 해서, 컴퓨터가 인식라지 못하게 해야 합니다.
 #  => 주석 crtl+/
 
+# 사용자 입력과 출력
+
 print('Hello, python!') # Hello, python! 을 출력합니다.
 print("5월 동안 잘 부탁드립니다. :) ") #  인삿말을 출력합니다.
 
@@ -13,3 +15,20 @@ next_age = age +1
 print('내년에는 ' + str(next_age) + '세입니다.')
 
 print(0)
+
+
+## end 옵션
+# 출력 후 한 줄 띄우지 않고, '끝'부분에 다른 문자 출력
+
+print("Hello world", end="!")
+
+word1 = "hello"
+word2 = "world"
+print(word1, end="  ")
+print(word2)
+
+print(word1, end="")
+print(word2)
+
+# hello  world
+# helloworld
