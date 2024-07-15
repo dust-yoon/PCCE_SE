4672537e0ef46e4b0e3d94897536fbd8e3e4ec62
JIYUN <popowjy12@konkuk.ac.kr>
2024-07-16 00:02:10 +0900

    feat: 강좌1 내용 정리

--- 1Day.py
+++ 1Day.py
@@ -23,15 +23,37 @@ student2 = {'name' : 'alex',
             'lisence' : True,
             'gender' : 'Male' }
 
-# 변수
+# 추가 학습 자료
+users = {
+    'total_user' : 3,
+    'information' : [
+        {'name' : 'Alex', 'age' : 3, 'license': True},
+        {'name' : 'June', 'age' : 7, 'license': False},
+        {'name' : 'Peter', 'age' : 4, 'license': False}
+    ]
+}
+
+
+## 변수
+# 알파벳, 언더바(_), 숫자로만 구성되어야 함
+# 첫 글자에는 숫자가 올 수 없음
+# 대소문자를 구별할 줄 알아야 함
+# 띄어쓰기가 필요할 경우 언더바로 표시
+# 예약어는 사용하지 않음
 
 name = '김건국'
 print('안녕하세요, ', name)
 
 # break = 'a'
 # # syntex highlighting : 예약어 의심 예약어 유의사항
-# print(break)
+# print(break) - 더이상 함수로서의 역할을 할 수 없게 됨.
+# 식별자 = Literal
 
+
+
+
+## 자료형(Data types)
+# 숫자형(Numeric type)-정수(Integer),실수(Float)//문자열(String type)//불린형(Boolean type)-참(True),거짓(False)
 
 # 숫자형
 
@@ -40,12 +62,13 @@ print(type(0)) # 자료형 확인 내장 함수 <class 'int'> 출력
 print(1)
 print(-2)
 
-# 2. 실수 (float)
+# 2. 실수 (float) 소수점이 포함되어 있음
 print(type(1.2))
 print(type(-3.14))
 
-# 어떤 자료형이 나오게 될까? -> float
+# 어떤 자료형이 나오게 될까? -> <class 'float'> 인데 <class 'int'>로 변환
 print(type(int(3.0)))
+
 
 # 문자열 (string -> str)
 
@@ -68,23 +91,32 @@ space = '   ' # 공백(white space)도 문자열
 
 print(type(test))
 
-# 불린형 - 명시적 형변환
+
+# 불린형
+# 공백은 False형 자료라고 볼 수 있음 (list 도 비어있으면 거짓 불린의 형태로 취급)
+# 조건문에서 유용하게 활용
+# 명시적 형변환은 Notion 참고(int(4.01)=4)
+
 num = 0
 print(num)
 print(type(num))
 print(bool(num))
+# False
 
 s = '123'
 print(s)
 print(type(s))
 print(bool(s))
+# True
 
 s = '0'
 print(s)
 print(type(s))
 print(bool(s))
+# True
 
 lst = [1]
 print(lst)
 print(type(lst))
 print(bool(lst))
+# True(리스트 안에 내용이 존재함)
