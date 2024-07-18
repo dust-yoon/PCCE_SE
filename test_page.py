d4bdd14505a97e6af968948920010d64478806b7
JIYUN <popowjy12@konkuk.ac.kr>
2024-07-18 16:36:51 +0900

    feat: 연습 파일

---
+++ test_page.py
@@ -0,0 +1,23 @@
+# bool1 = 2 < 5
+# bool2 = True == "True" # bool 값과 연산자 값이라서 같지 않음
+
+# if bool1 :
+#     print("출력되게 하세요.")
+
+# if bool2 :
+#     print("출력되지 않게 하세요.")
+
+# number = int(input("짝수인가요?: "))
+# condition = number % 2 == 0
+# condition2 = number != 0
+
+# if condition and condition2:
+#     print("짝수입니다.")
+# else:
+#     if condition:
+#         print("0입니다.")
+#     else:
+#         print("홀수입니다.")
+
+for i in range(1, 11):
+    print(i, end=" ")
