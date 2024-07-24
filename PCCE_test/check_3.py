9f6f95cbcbf3cd3628d0632c88e17542bf343b43
JIYUN <popowjy12@konkuk.ac.kr>
2024-07-24 18:58:56 +0900

    test

---
+++ PCCE_test/check_3.py
@@ -0,0 +1,13 @@
+def solution(snippet, message):
+    answer = ''
+    snip = {}
+    for i in range(len(snippet)):
+        snip[str(snippet[i][0])] = snippet[i][1]
+    
+    for i in range(len(snip)):
+        if i 
+        message.replace(str(list(snip.keys())[i]), str(snip[list(snip.keys())[i]]))
+        
+    return message
+
+print(solution([["m", "me"]], "Love m"))
