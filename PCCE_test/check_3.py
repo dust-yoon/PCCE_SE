2b13898f5b2c2a2b441d2f26a75532c1a1ffd64b
JIYUN <popowjy12@konkuk.ac.kr>
2024-07-25 10:12:41 +0900

    feat: 모의고사 문항

--- PCCE_test/check_3.py
+++ PCCE_test/check_3.py
@@ -1,13 +1,35 @@
 def solution(snippet, message):
     answer = ''
     snip = {}
+
+    message = message + ' '
+
     for i in range(len(snippet)):
         snip[str(snippet[i][0])] = snippet[i][1]
     
     for i in range(len(snip)):
-        if i 
-        message.replace(str(list(snip.keys())[i]), str(snip[list(snip.keys())[i]]))
-        
+        if message.find(list(snip.keys())[i] + " ") == -1:
+            continue
+        else:
+            message = message.replace(list(snip.keys())[i] + " " , snip[list(snip.keys())[i]] + " ")
+
+    message.strip(message[len(message)-1])
+
     return message
 
-print(solution([["m", "me"]], "Love m"))
+print(solution([["msg", "message"], ["m", "me"], ["s", "see"], ["g", "group"]], "msg"))
+print(solution([["IMO", "In my opinion"]], "IMO, IMO"))
+print(solution([["IMO,", "In my opinion,"], 
+                ["AYS?", "Are you serious?"], 
+                ["TTYL.", "Talk to you later."]], 
+                "IMO, it does not look so good. AYS? TTYL."))
+
+print("In my opinion, it does not look so good. Are you serious? Talk to you later.")
+
+# 띄어쓰기 추가해야함
+# 자료형 변환
+# 라이브러리, 리스트, 등등 변환과 내장 함수 파악
+# 오답 정리
+
+# snip = {'m' : 'me'}
+
